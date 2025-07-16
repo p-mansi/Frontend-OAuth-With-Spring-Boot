57d05994c36fc475bce98d297b0296df498dd337
p-mansi <mgpadia@gmail.com>
2025-07-16 22:04:44 +0530

    Initial Commit

---
+++ src/main/java/edu/charusat/BookMyResource/BookMyResourceApplication.java
@@ -0,0 +1,13 @@
+package edu.charusat.BookMyResource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookMyResourceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookMyResourceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/charusat/BookMyResource/BookMyResourceApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.charusat.BookMyResource;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookMyResourceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
