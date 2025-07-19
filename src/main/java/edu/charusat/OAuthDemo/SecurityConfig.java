88a8df16eae67945134779f0eeeec994ef3e3668
p-mansi <mgpadia@gmail.com>
2025-07-19 13:18:54 +0530

    OAuth Implematation done

--- src/main/java/edu/charusat/OAuthDemo/SecurityConfig.java
+++ src/main/java/edu/charusat/OAuthDemo/SecurityConfig.java
@@ -2,7 +2,6 @@ package edu.charusat.OAuthDemo;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -20,7 +19,7 @@ public class SecurityConfig {
         http.csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authorizeRequests -> authorizeRequests.anyRequest().authenticated())
                 // .formLogin(form -> form.defaultSuccessUrl("/hello", true));
-                .oauth2Login(oauth2 -> oauth2.defaultSuccessUrl("/hello", true));
+                .oauth2Login(oauth2 -> oauth2.defaultSuccessUrl("http://localhost:3000/dashboard", true));
 
         return http.build();
     }
